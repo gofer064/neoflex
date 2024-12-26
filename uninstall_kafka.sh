88818769daa3cdd3f386c3e71828f382f40b1ee0
Alex <116117412+gofer064@users.noreply.github.com>
2024-12-26 12:10:26 +0400

    Create uninstall_kafka.sh
    Add uninstall script

---
+++ uninstall_kafka.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#--------------------------------------------------------------------
+# Script to uninstall Kafka+Zookeeper on Linux
+# Tested on Ubuntu 22.04, 24.04
+# Developed by Alexey Beloglazov in 2024
+#https://github.com/gofer064/neoflex/blob/main/uninstall_kafka.sh
+#--------------------------------------------------------------------
+
+systemctl stop zookeeper
+systemctl stop kafka
+
+systemctl disable zookeeper
+systemctl disable kafka
+
+rm /etc/systemd/system/kafka.service
+rm /etc/systemd/system/zookeeper.service
+
+
+systemctl daemon-reload
+
+rm -rf /tmp/kafka*
+rm -rf /opt/kafka
+rm -rf /var/lib/kafka
+rm -rf /var/lib/zookeeper
+rm -rf /tmp/lib/kafka/kafka-logs
+rm -rf /var/log/kafka
+rm -rf /var/log/zookeeper
+
+userdel kafka
